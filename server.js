98f37ef07bb526f88718611dce0afcf8776cfcdc
Erik Larsen <erik@greystar.local>
2018-02-21 22:16:48 +0100

    added eslint

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
-// server.js
 
-// BASE SETUP
-// =============================================================================
-
-// call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const readline = require('readline');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 5555;        // set our port
+const port = process.env.PORT || 6666;        // set our port
 
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'ho! welcome to our api!' });   
+router.get('/', function (req, res) {
+    res.json({ message: 'ho! welcome to our api!' });
 });
 
 // more routes for our API will happen here
@@ -32,10 +28,18 @@ app.use('/api/v1', router);
 
 // START THE SERVER
 // =============================================================================
-const sever = app.listen(port);
+const server = app.listen(port);
+
 console.log('Magic happens on port ' + port);
 
-// listen for an event, to shut down the server and free the port
-var handler = function() {
-    server.close();
-  };
+
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+process.stdin.on('keypress', (str, key) => {
+    if (key.ctrl && key.name === 'c') {
+        server.close();
+        process.exit();
+    } else {
+        console.log('Press "ctrl-c" to exit');
+    }
+});
