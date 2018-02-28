2715cea8f459c5397107e4da9430b232e641e0e4
Erik Larsen <erik@greystar.local>
2018-02-28 21:28:53 +0100

    Moved integration tests

--- test/suite.js
+++ test/suite.js
@@ -1,89 +1,9 @@
-const assert = require("assert");
-const request = require('supertest');
-const app = require('../server/server').app;
-const db = require('../server/server').MongoWrapper;
-
-require('./unit/seeder')
-
 /* Test suite*/
-describe('Turplan.no API Integration Tests', function () {
 
-    /* Spin up mongodb and populate with data, before conducting any integration tests */
-    before(function (done) {
-        db.connect();
-        app.once("ready", function () {
-            done();
-        })
-    });
-
-    /* Clean up and close server after tests */
-    after(function () {
-        app.close();
-    });
-
-    /* Integration tests */
-
-    describe('GET /api/v1', function () {
-        it('should return 200', function (done) {
-            request(app)
-                .get('/api/v1/')
-                .set('Accept', 'application/json')
-                .expect('Content-Type', /json/)
-                .expect(200, done);
-        });
-    });
-
-    describe('GET /api/v1/collections', function () {
-        it('should return 200', function (done) {
-            request(app)
-                .get('/api/v1/collections')
-                .set('Accept', 'application/json')
-                .expect('Content-Type', /json/)
-                .expect(200)
-                .end(done);
-        });
-        it('should return expected JSON', function (done) {
-            request(app)
-                .get('/api/v1/collections')
-                .expect('Content-Type', /json/)
-                .expect({ "Items": ["trips", "users"] })
-                .end(function (err, res) {
-                    if (err) return done(err);
-                    done();
-                });
-        });
-    });
-
-    describe('GET /api/v1/{collection} that doesnt exist', function () {
-        it('should return 404', function (done) {
-            request(app)
-                .get('/api/v1/doesntexists', {})
-                .expect('Content-Type', /json/)
-                .expect(404)
-                .end(function (err, res) {
-                    if (err) return done(err);
-                    done();
-                });
-        });
-    });
-
-    describe('GET /api/v1/{collection}/{id} that doesnt exist', function () {
-        it('should return 404', function (done) {
-            request(app)
-                .get('/api/v1/trips/1', {})
-                .expect('Content-Type', /json/)
-                .expect(404)
-                .end(function (err, res) {
-                    if (err) return done(err);
-                    done();
-                });
-        });
-    });
+describe('Turplan.no API Unit Tests', function () {
+    return require('./unit/seeder');
 });
 
-
-
-
-
-
-
+describe('Turplan.no API Integration Tests', function () {
+    return require('./integration');
+});
