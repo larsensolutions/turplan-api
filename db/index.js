37dc0f304e10acdbfb2911066172892676769ac4
Erik Andreas Larsen <eriklarsen.post@gmail.com>
2018-03-12 23:18:34 +0100

    Fixed collection routing to get collection documents

--- db/index.js
+++ db/index.js
@@ -9,36 +9,53 @@ const addr = "mongo", port = "27017", db = "test";
 const uri = `mongodb://${addr}:${port}`;
 
 class MongoWrapper {
-    constructor(uri){
+    constructor(uri) {
         this.data = require('./mock.json');
         this.uri = uri;
     }
-    onConnect(err, client, resolve, reject){
+    onConnect(err, client, resolve, reject) {
         if (err) { throw err; }
         this.client = client;
         this.db = client.db(db);
 
-        Seeder.seed(this.db).with(this.data).then((ok)=>{
+        Seeder.seed(this.db).with(this.data).then((ok) => {
             console.log(ok.status);
             this.collectionNames = ok.collectionNames;
             resolve();
-        }, (error)=>{
+        }, (error) => {
             console.log(error);
         });
     }
-    getCollectionNames(req, res){
+    getCollectionNames(req, res) {
         var names = [];
-        this.db.listCollections().toArray(function(err, items) {
+        this.db.listCollections().toArray(function (err, items) {
             names = items.map(item => {
                 return item.name;
             });
-            res.status(200).json({Items: names});
+            res.status(200).json({ Items: names });
         });
     }
-    disconnect(){
+    getDocuments(req, res) {
+        var options = {
+            "limit": Number(req.query.limit) || 20,
+            "skip": Number(req.query.skip) || 0
+        }
+        var cursor = this.db.collection(req.params.collection).find({}, options);
+        cursor.count(false, (err, total) => {
+            cursor.toArray((err, docs) => { // Loads all into memory, should avoid this
+                if (err) {
+                    res.status(500).json(new Error(err));
+                } else {
+                    res.status(200).json({ "documents": docs, "count": docs.length, "total": total });
+                }
+            });
+        });
+    }
+
+    disconnect() {
         this.client.close();
     }
-    connect(){
+    connect() {
         return new Promise((resolve, reject) => {
             MongoClient.connect(this.uri, (err, client) => this.onConnect(err, client, resolve, reject));
         });
@@ -50,8 +67,8 @@ const mongo = new MongoWrapper(uri);
 module.exports = mongo;
 
 module.exports.param = (req, res, next, col) => {
-    if(!mongo.collectionNames.includes(col)){
-        return res.status(404).json({message: `Collection ${col} not found.`});
+    if (!mongo.collectionNames.includes(col)) {
+        return res.status(404).json({ message: `Collection ${col} not found.` });
     }
     next();
 }
--- server/router.js
+++ server/router.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const mongoDB = require('../db');
 
-var router = express.Router();   
+var router = express.Router();
 
 /* GET  / */
 router.get('/', function (req, res) {
-    res.status(200).json({ message: 'You better believe it!'});
+    res.status(200).json({ message: 'You better believe it!' });
 });
 
 /* GET collections */
@@ -16,27 +16,27 @@ router.get('/collections', function (req, res) {
 /* GET all /{collection} */
 router.param('collection', mongoDB.param);
 router.get('/:collection', function (req, res) {
-    switch (req.method){
+    switch (req.method) {
         case 'GET':
-            res.status(200).json({message:"To be"});
+            mongoDB.getDocuments(req, res);
             break;
         default:
-            res.status(405).json({message: `HTTP Method #${req.method.toUpperCase()} Not Allowed`});
+            res.status(405).json({ message: `HTTP Method #${req.method.toUpperCase()} Not Allowed` });
     }
 });
 
 /* GET single /{collection}/{id} */
 router.param('id', mongoDB.param);
 router.get('/:collection/:id', function (req, res) {
-    switch (req.method){
+    switch (req.method) {
         case 'GET':
-            res.status(200).json({message:"Or not to be"});
+            res.status(200).json({ message: "Or not to be" });
             break;
         default:
-            res.status(405).json({message: `HTTP Method #${req.method.toUpperCase()} Not Allowed`});
+            res.status(405).json({ message: `HTTP Method #${req.method.toUpperCase()} Not Allowed` });
     }
 });
 
 module.exports = (req, res, next) => {
-   router(req,res,next);
+    router(req, res, next);
 };
